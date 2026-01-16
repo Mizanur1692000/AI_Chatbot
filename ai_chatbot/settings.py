e05189a3948646c7802f13ecbc8fc7588bcbc38e
Mizanur1692000 <mizanur.rahman.1cse@gmail.com>
2026-01-17 03:07:32 +0600

    Completed the chatboty

--- ai_chatbot/settings.py
+++ ai_chatbot/settings.py
@@ -13,17 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
 load_dotenv()  # Load environment variables from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -47,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatbot',
-    'rest_framework',  
 ]
 
 MIDDLEWARE = [
